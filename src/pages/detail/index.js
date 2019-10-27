2d30e1d7dcf3a341e3834a0a99954919f39f8a8f
eric <eric@pql.local>
2019-10-27 11:15:44 +0800

    详情页面布局

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import {
+    DetailWrapper,
+    Header,
+    Content
+} from './style';
 
 class Detail extends Component {
     render () {
         return (
-            <div>Detail~</div>
+            <DetailWrapper>
+                <Header>衡水中学</Header>
+                <Content>
+                    <img src="//upload.jianshu.io/admin_banners/web_images/4759/dbf72c4d562f220fe8032de0d9daa3df190f3dc9.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"/>
+                    <p>`"topicList"["id"1,"title""社会热点","imgUrl""//upload.jianshu.io/admin_banners/web_images/4759/dbf72c4d562f220fe8032de0d9daa3df190f3dc9.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"},"id"2,"title""时事政治","imgUrl""//upload.jianshu.io/admin_banners/web_images/4759/dbf72c4d562f220fe8032de0d9daa3df190f3dc9.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"}],"articleList"["id"1,"title""胡歌","desc""描述","imgUrl""//upload.jianshu.io/admin_banners/web_images/4759/dbf72c4d562f220fe8032de0d9daa3df190f3dc9.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"},"id"2,"title""胡歌","desc""描述","imgUrl""//upload.jianshu.io/admin_banners/web_images/4759/dbf72c4d562f220fe8032de0d9daa3df190f3dc9.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"},"id"3,"title""胡歌","desc""描述","imgUrl""//upload.jianshu.io/admin_banners/web_images/4759/dbf72c4d562f220fe8032de0d9daa3df190f3dc9.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"}],"recommendList"["id"1,"imgUrl""//upload.jianshu.io/admin_banners/web_images/4759/dbf72c4d562f220fe8032de0d9daa3df190f3dc9.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"},"id"2,"imgUrl""//upload.jianshu.io/admin_banners/web_images/4759/dbf72c4d562f220fe8032de0d9daa3df190f3dc9.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"}]}`</p>
+                </Content>
+            </DetailWrapper>
         )
     }
 }
